9e0b7e47c8601ae2251b972dd1e08d841d4c7a1c
Ihor Triapitsyn <tryapitsin1990@gmail.com>
2021-01-09 19:38:46 +0200

    add table

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,28 +1,50 @@
 import React from "react";
 
 const Weather = ({weather}) => {
-
     return (
         <div>
             {!weather ?
                 <div>...Loading</div>
                 : (
                     <div>
-                        <div>City: {weather.name}</div>
+
 
                         {(typeof weather.main != 'undefined') ? <>
-                                <div className='temp'>
-                                   Temperature:  {Math.round(weather.main.temp)}°c
-                                </div>
-                                <div>Speed wind: {weather.wind.speed} м/с</div>
+                                <table className="table ">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">Населенный пункт</th>
+                                        <th scope="col">Скорость ветра</th>
+                                        <th scope="col">Облачность</th>
+                                        <th scope="col">Температура воздуха</th>
+                                        <th scope="col">Давление</th>
+                                        <th scope="col">Влажность</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>{weather.name}</td>
+                                        <td>{weather.wind.speed} м/с</td>
+                                        <td>
+                                            {weather.weather[0].description}
+                                            <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt=""/>
+                                        </td>
+                                        <td>{weather.main.temp } &#176;C</td>
+                                        <td>{weather.main.pressure} мм.рт.ст.</td>
+                                        <td>{weather.main.humidity} %</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
                             </>
                             : <div></div>
                         }
+
                     </div>
                 )
             }
         </div>
     )
+
 }
 
 export default Weather;
